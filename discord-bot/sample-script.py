add4830bc49e3d5b881c3f9352f10c912f8d12f4
Kalpit Shah <kalpit@atliq.com>
2023-08-30 20:20:19 +0530

    Sample python script added for deployment

---
+++ discord-bot/sample-script.py
@@ -0,0 +1,17 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"Hello, World!")
+
+def run_server(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8000):
+    server_address = ("", port)
+    httpd = server_class(server_address, handler_class)
+    print(f"Server is running on port {port}")
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run_server()
