cd436301d391b7ca5a9e474234ceba8735bb2840
Yagnik Poshiya <89079815+yagnikposhiya@users.noreply.github.com>
2023-09-01 01:58:00 +0530

    update main.py

--- discord-bot/main.py
+++ discord-bot/main.py
@@ -6,6 +6,14 @@ Charotar University of Science and Technology.
 """
 
 import bot
+import flask
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/health')
+def health():
+    return "OK", 200
 
 if __name__=='__main__':
     bot.runCREDPAssistant()
+    app.run()
