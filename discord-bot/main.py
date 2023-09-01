d9962477d38a0b64f4227e42a94875272ef501a6
yagnikposhiya <yagnikposhiya97275@gmail.com>
2023-09-01 12:12:45 +0530

    removed flask app

--- discord-bot/main.py
+++ discord-bot/main.py
@@ -6,14 +6,6 @@ Charotar University of Science and Technology.
 """
 
 import bot
-import flask
-from flask import Flask
-
-app = Flask(__name__)
-@app.route('/health')
-def health():
-    return "OK", 200
 
 if __name__=='__main__':
     bot.runCREDPAssistant()
-    app.run()
