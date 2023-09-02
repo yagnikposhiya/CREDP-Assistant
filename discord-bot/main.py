8820ad827c4e3d28386807dc25cb6bbd2799ce97
yagnikposhiya <yagnikposhiya97275@gmail.com>
2023-09-02 11:08:55 +0000

    multiprocessing approach tried

--- discord-bot/main.py
+++ discord-bot/main.py
@@ -7,7 +7,14 @@ Charotar University of Science and Technology
 
 import bot
 import dbping
+import multiprocessing
 
 if __name__=='__main__':
-  dbping.dbPing()
-  bot.runCREDPAssistant()
+  database_ping_process = multiprocessing.Process(target=dbping.dbPing())
+  discord_bot_process = multiprocessing.Process(target=bot.runCREDPAssistant())
+
+  database_ping_process.start()
+  discord_bot_process.start()
+  
+  # dbping.dbPing()
+  # bot.runCREDPAssistant()
